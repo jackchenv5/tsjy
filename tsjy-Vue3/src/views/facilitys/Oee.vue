2dc4cb56a44a6168695e456dc26527d43eeefeb6
chenchengf <chenchengf@163.com>
2025-06-11 00:04:30 +0800

    更新OEE视图

--- tsjy-Vue3/src/views/facilitys/Oee.vue
+++ tsjy-Vue3/src/views/facilitys/Oee.vue
@@ -1,128 +1,86 @@
 <template>
   <div class="container">
     <div class="header">
-      <div class="header-item">
-        <div style="height: 70%; width: 70%">
-          <el-progress type="dashboard" :percentage="80" :stroke-width="20">
-            <template #default="{ percentage }">
-              <p class="percentage-value">{{ percentage }} m/min</p>
-              <p class="percentage-label">印刷速度</p>
-            </template>
-          </el-progress>
+      <div class="header-card-container">
+        <div
+          style="height: 100%; display: flex; margin-left: 20px; flex-grow: 1"
+        >
+          <div class="progress-container">
+            <el-progress
+              width="90"
+              type="dashboard"
+              :percentage="80"
+              stroke-width="16"
+              class="custom-progress"
+            >
+              <template #default="{ percentage }">
+                <p class="percentage-value">{{ percentage }}</p>
+                <span class="percentage-label">OEE</span>
+              </template>
+            </el-progress>
+          </div>
+
+          <div
+            style="
+              margin-left: 20px;
+              display: flex;
+              flex-direction: column;
+              height: 100%;
+              justify-content: center;
+              gap: 10px;
+              align-items: center;
+            "
+          >
+            <div style="font-size: 1.6rem; font-weight: bold">
+              当前设备运行正常
+            </div>
+            <div style="font-size: 1.2rem; color: #999">已无故障运行 30 天</div>
+          </div>
         </div>
-      </div>
-      <div class="header-item">
-        <div style="height: 70%; width: 70%">
-          <el-progress type="dashboard" :percentage="80" :stroke-width="20">
-            <template #default="{ percentage }">
-              <p class="percentage-value">{{ percentage }} kw</p>
-              <p class="percentage-label">加热功率</p>
-            </template>
-          </el-progress>
-        </div>
-      </div>
-      <div class="header-item">
-        <div style="height: 70%; width: 70%">
-          <el-progress type="dashboard" :percentage="80" :stroke-width="20">
-            <template #default="{ percentage }">
-              <p class="percentage-value">{{ percentage }} kw</p>
-              <p class="percentage-label">设备功率</p>
-            </template>
-          </el-progress>
-        </div>
-      </div>
-      <div class="header-item">
-        <div style="height: 70%; width: 70%">
-          <el-progress type="dashboard" :percentage="80" :stroke-width="20">
-            <template #default="{ percentage }">
-              <p class="percentage-value">{{ percentage }} kw</p>
-              <p class="percentage-label">排风功率</p>
-            </template>
-          </el-progress>
-        </div>
-      </div>
-      <div class="header-item">
-        <div style="height: 70%; width: 70%">
-          <el-progress type="dashboard" :percentage="80" :stroke-width="20">
-            <template #default="{ percentage }">
-              <p class="percentage-value">{{ percentage }} /A</p>
-              <p class="percentage-label">用电量</p>
-            </template>
-          </el-progress>
-        </div>
-      </div>
-      <div class="last-header-item">
-        <div class="header-info-item">
-          <p>柜内温度 0 C</p>
-        </div>
-        <div class="header-info-item">
-          <p>车间温度 0 C</p>
-        </div>
-        <div class="header-info-item">
-          <p>柜内湿度 0 C</p>
-        </div>
-        <div class="header-info-item">
-          <p>车间湿度 0 C</p>
+        <div style="width: 32%; display: flex; gap: 5vw">
+          <div class="text-container">
+            <span class="text-label"> 运行时长 </span
+            ><span class="text-content">12/20</span>
+          </div>
+          <div class="text-container">
+            <span class="text-label"> 产量 </span
+            ><span class="text-content">800</span>
+          </div>
+          <div class="text-container">
+            <span class="text-label"> 良品率 </span
+            ><span class="text-content">80%</span>
+          </div>
         </div>
       </div>
     </div>
     <div class="mid">
-      <el-card class="mid-card">
-        <template #header>
-          <div class="card-header">
-            <span>印刷组</span>
-          </div>
-        </template>
-        <p><span>温度：</span>20℃<span></span></p>
-        <p><span>进风风速：</span>20℃<span></span></p>
-        <p><span>回风风速：</span>20℃<span></span></p>
+      <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+        <el-tab-pane label="时长" name="first"></el-tab-pane>
+        <el-tab-pane label="产量" name="second"></el-tab-pane>
+        <el-tab-pane label="良品率" name="third"></el-tab-pane>
+      </el-tabs>
+      <BarChart />
+    </div>
+    <div class="footer">
+      <el-card class="footer-card" header="运行时长">
+        <PieChartOne />
       </el-card>
-      <el-card class="mid-card">
-        <template #header>
-          <div class="card-header">
-            <span>天桥一区</span>
-          </div>
-        </template>
-        <p><span>温度：</span>20℃<span></span></p>
-        <p><span>风速：</span>20℃<span></span></p>
+      <el-card class="footer-card" header="产量">
+        <PieChartTwo />
       </el-card>
-      <el-card class="mid-card">
-        <template #header>
-          <div class="card-header">
-            <span>天桥二区</span>
-          </div>
-        </template>
-        <p><span>温度：</span>20℃<span></span></p>
-        <p><span>风速：</span>20℃<span></span></p>
-      </el-card>
-      <el-card class="mid-card">
-        <template #header>
-          <div class="card-header">
-            <span>天桥三区</span>
-          </div>
-        </template>
-        <p><span>温度：</span>20℃<span></span></p>
-        <p><span>风速：</span>20℃<span></span></p>
+      <el-card class="footer-card" header="良品率">
+        <PieChartThree />
       </el-card>
     </div>
     <!-- 结构不变 -->
-    <div class="footer">
-      <div class="footer-item" style="left: 15%; top: 18%">
-        <p class="footer-info">放牵张力：10</p>
-      </div>
-      <div class="footer-item" style="left: 33%; top: 18%">
-        <p class="footer-info">收牵张力：10</p>
-      </div>
-      <div class="footer-item" style="left: 52%; top: 25%">
-        <p class="footer-info">放料张力：10</p>
-      </div>
-      <div class="footer-item" style="left: 70%; top: 25%">
-        <p class="footer-info">收料张力：10</p>
-      </div>
-    </div>
   </div>
 </template>
-
+<script setup>
+import BarChart from './oeeChart/BarChart.vue'
+import PieChartOne from './oeeChart/PieChart1.vue'
+import PieChartTwo from './oeeChart/PieChart2.vue'
+import PieChartThree from './oeeChart/PieChart3.vue'
+</script>
 <style scoped lang="scss">
 * {
   box-sizing: border-box;
@@ -138,71 +96,47 @@
 
 .header {
   border: 1px solid wihte;
-  height: 20%;
-  display: flex;
+  height: 15%;
+  background-color: #fff;
 }
 
-.last-header-item {
-  flex: 1;
+.header-card-container {
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
-  flex-wrap: wrap;
+  height: 100%;
 }
-
-.header-item {
-  min-width: 200px;
-  border: 1px solid white;
+.progress-container {
   display: flex;
-  justify-content: center; // 水平居中
-  align-items: center; // 垂直居中
+  justify-content: center;
+  align-items: center;
 }
-
-.header-info-item {
-  width: 48%;
-  height: 48%;
-  background-color: #eeeeef;
+.text-container {
   display: flex;
-  justify-content: center; // 水平居中
-  align-items: center; // 垂直居中
+  flex-direction: column;
+  gap: 5px;
 }
-
+.text-label {
+  color: #999;
+}
+.text-content {
+  font-size: 1.6rem;
+  font-weight: bold;
+}
 .mid {
-  display: flex;
-  gap: 1vw;
-  height: 30%;
-  min-height: 0px;
-  flex: 1;
-}
-.mid-card {
-  min-width: 19%;
-  flex: 1;
-}
-.last-mid-card {
-  min-width: 0;
-  flex: 1;
-}
-
-.footer {
-  height: 50%;
-  position: relative; // 关键：设为定位父容器
-  background-image: url('/device.jpg');
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-}
-
-.footer-item {
-  position: absolute; // 关键：绝对定位
-  min-width: 200px;
-}
-
-.footer-info {
-  margin: 0; // 移除原有边距
-  padding: 8px;
-  font-size: 2rem;
+  margin-top: 10px;
   background-color: white;
-  border-radius: 7px;
-  width: 80%;
+}
+.footer {
+  margin-top: 10px;
+  flex-grow: 1;
+  background-color: #eee;
+  display: flex;
+  justify-content: space-between;
+  align-items: stretch; /* 添加此属性，使卡片高度相同且撑满 */
+}
+.footer-card {
+  width: 33%;
+  height: 100%;
 }
 </style>
---
+++ tsjy-Vue3/src/views/facilitys/oeeChart/PieChart1.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="chart-container" ref="chartRef"></div>
+</template>
+<script setup>
+import { ref, onMounted } from 'vue'
+import * as echarts from 'echarts'
+const chartRef = ref(null)
+let chartInstance = null
+const initChart = () => {
+  chartInstance = echarts.init(chartRef.value)
+
+  chartInstance.setOption({
+    tooltip: {
+      trigger: 'item',
+    },
+    legend: {
+      bottom: '1%',
+      left: 'center',
+    },
+    series: [
+      {
+        color: ['#5ab1ef', '#b6a2de', '#67e0e3', '#2ec7c9'],
+        name: '时长',
+        type: 'pie',
+        radius: ['40%', '70%'],
+        avoidLabelOverlap: false,
+        itemStyle: {
+          borderRadius: 10,
+          borderColor: '#fff',
+          borderWidth: 2,
+        },
+        label: {
+          show: false,
+          position: 'center',
+        },
+        emphasis: {
+          label: {
+            show: true,
+            fontSize: '12',
+            fontWeight: 'bold',
+          },
+        },
+        labelLine: {
+          show: false,
+        },
+        data: [
+          { value: 1048, name: '运行时长' },
+          { value: 735, name: '待机时长' },
+        ],
+        animationType: 'scale',
+        animationEasing: 'exponentialInOut',
+        animationDelay: function () {
+          return Math.random() * 100
+        },
+      },
+    ],
+  })
+}
+onMounted(() => {
+  initChart()
+})
+</script>
+<style scoped>
+.chart-container {
+  width: 100%;
+  height: 300px;
+}
+</style>
